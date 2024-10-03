45021effd0c695ec430daa8c790f763429bedb8a
mishalalshahari <mishalalshahari@gmail.com>
2024-10-03 18:51:55 +0530

    basic spring and thymeleaf setup

---
+++ src/main/java/com/mypeople/Controller/PageController.java
@@ -0,0 +1,13 @@
+package com.mypeople.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+    @RequestMapping("/home")
+    public String home() {
+        System.out.println("Home page handler");
+        return "home";
+    }
+}
---
+++ src/test/java/com/mypeople/MyPeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPeopleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
