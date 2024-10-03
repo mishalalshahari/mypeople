b4c5cdfcb8321db861377685de2a7ee567f4123b
mishalalshahari <mishalalshahari@gmail.com>
2024-10-03 21:38:16 +0530

    created more routes to understand fragments

--- src/main/java/com/mypeople/Controller/PageController.java
+++ src/main/java/com/mypeople/Controller/PageController.java
@@ -1,14 +1,31 @@
 package com.mypeople.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
     @RequestMapping("/home")
-    public String home() {
+    public String homePage(Model model) {
 
         System.out.println("Home page handler");
+        //sending data to view
+        model.addAttribute("name","MyPeople");
         return "home";
+    }
+
+    //about route
+    @RequestMapping("/about")
+    public String aboutPage() {
+        System.out.println("About page is loading");
+        return "about";
+    }
+
+    //about route
+    @RequestMapping("/service")
+    public String servicePage() {
+        System.out.println("Service page is loading");
+        return "service";
     }
 }
