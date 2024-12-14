91a078753b424039e08e6c3e46f93a909c7e6855
Mishal Al Shahari <mishalalshahari@gmail.com>
2024-12-14 23:36:10 +0530

    add contact

--- src/main/java/com/mypeople/Entities/Contact.java
+++ src/main/java/com/mypeople/Entities/Contact.java
@@ -1,11 +1,19 @@
 package com.mypeople.Entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Contact {
 
     @Id
